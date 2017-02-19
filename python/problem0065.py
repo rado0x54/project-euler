5a91c91a729f66a27d8c12b4fc4697a779492e7d
Martin Riedel <web@riedel-privat.de>
2017-02-19 17:57:34 +0100

    Refactored Solution 65

--- python/problem0065.py
+++ python/problem0065.py
@@ -9,16 +9,12 @@ def get_nth_convergent_e(n):
     # generate n-1 terms
     faction_terms = [2 * (x + 2) // 3 if (x + 2) %
                      3 == 0 else 1 for x in range(n - 1)]
-    return Fraction(2, 1) + get_continued_fractions(faction_terms)
 
-
-def get_continued_fractions(faction_terms):
     lowest_fraction = Fraction()  # == 0
     for i in reversed(faction_terms):
         lowest_fraction = Fraction(1, Fraction(i) + lowest_fraction)
 
-    return lowest_fraction
-
+    return Fraction(2, 1) + lowest_fraction
 
 def problem65(n):
     """Problem 65 - Convergents of e"""
